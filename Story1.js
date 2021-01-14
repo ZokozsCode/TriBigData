0457bb0310308558905958b53657b295cf42fcae
Angel <Angelmoreau95@gmail.com>
2021-01-14 16:58:16 +0100

    debut story3

--- Story1.js
+++ Story1.js
@@ -3,3 +3,7 @@ var args = process.argv;
 if (args[2] !== "-action") {
     return console.log("[ERROR] - Premier argument doit toujours être \"-action\".");
 }
+
+console.time(("start"))
+
+console.timeEnd(("start"))
---
+++ story3.js
@@ -0,0 +1,3 @@
+console.time(("start"))
+
+console.timeEnd(("start"))
