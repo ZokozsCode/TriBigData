1ab5beb7432fa31b7f7c68e700eeb5cc223d76dd
Angel <Angelmoreau95@gmail.com>
2021-01-14 19:47:56 +0100

    ajout story 3

--- Story1.js
+++ Story1.js
@@ -1,9 +1,17 @@
+// Story 1 //
+
 var args = process.argv;
 
 if (args[2] !== "-action") {
     return console.log("[ERROR] - Premier argument doit toujours être \"-action\".");
 }
 
+
+
+
+// Story 3 //
+
 console.time(("start"))
+
 
 console.timeEnd(("start"))
